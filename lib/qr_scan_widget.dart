4ead1ef0740e0dc4f1b8afb8e0d32582a79001f0
Pascal <pascalneubeck@gmx.de>
2025-05-11 20:32:37 +0200

    implemented first version of QR code scan and parsing.

--- lib/qr_scan_widget.dart
+++ lib/qr_scan_widget.dart
@@ -1,3 +1,4 @@
+import 'package:basic_utils/basic_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
@@ -9,19 +10,60 @@ class QrScanWidget extends StatefulWidget {
 }
 
 class _QrScanWidget extends State<QrScanWidget> {
+  bool _dataScanned = false;
+
   void _onDetected(BarcodeCapture result) {
-    print(result.barcodes.first.rawValue);
+    try {
+      setState(() {
+        _dataScanned = true;
+      });
+      var scannedString = result.barcodes.first.rawValue;
+      if (scannedString == null || scannedString.isEmpty) {
+        throw 'Scanned data was null... something is off.';
+      }
+      List<String> contactData = scannedString.split('|');
+      if (contactData.length != 3) {
+        throw 'Invalid number of sections in scanned string... something is off.';
+      }
+      var scannedUserId = contactData[0];
+      var scannedUserName = contactData[1];
+      var scannedPublicKey = CryptoUtils.rsaPublicKeyFromPem(contactData[2]);
+    } catch (error) {
+      if (mounted) {
+        showDialog(
+          context: context,
+          builder:
+              (BuildContext context) => AlertDialog(
+                title: Text('Fehler'),
+                content: Text(
+                  'Die Aktion konnte nicht abgeschlossen werden. Versuchen Sie es später erneut\nDer Fehler war:\n$error',
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('Ok'),
+                  ),
+                ],
+              ),
+        );
+      }
+    }
   }
 
   @override
   Widget build(context) {
     return Scaffold(
       body: Expanded(
-        child: MobileScanner(
-          onDetect: (result) {
-            _onDetected(result);
-          },
-        ),
+        child:
+            !_dataScanned
+                ? MobileScanner(
+                  onDetect: (result) {
+                    _onDetected(result);
+                  },
+                )
+                : CircularProgressIndicator(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
       ),
     );
   }
